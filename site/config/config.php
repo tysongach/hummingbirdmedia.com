e46a718aa8e4a664818d3142586dece2d0d66c7e
Tyson Gach <tyson@thoughtbot.com>
2016-07-05 22:08:51 -0400

    Update config

--- site/config/config.php
+++ site/config/config.php
@@ -2,4 +2,6 @@
 
 c::set("autopublish.templates", "press-releases");
 c::set("content.file.extension", "md");
+c::set("error", "not-found");
+c::set("markdown.extra", true);
 c::set("timezone", "America/New_York");
